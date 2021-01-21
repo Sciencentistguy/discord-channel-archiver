8024dcc16aa133af6d46001e8c4eb9e8a28c015c
Jamie Quigley <jamie@quigley.xyz>
2021-01-21 15:27:39 +0000

    Improved command syntax

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 mod json;
 
 use std::env;
+use std::str::FromStr;
 
+use lazy_static::lazy_static;
 use log::*;
+use regex::Regex;
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
+    model::{channel::Message, gateway::Ready, id::ChannelId},
     prelude::*,
 };
 use structopt::StructOpt;
+
+static PATH: &str = "/dev/shm";
 
 #[tokio::main]
 async fn main() {
@@ -54,23 +59,51 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!archive" {
-            let channel = msg
-                .channel_id
-                .to_channel(&ctx)
-                .await
-                .expect("Channel not found")
-                .guild()
-                .expect("Invalid channel type");
+        if msg.content.starts_with("!archive") {
+            lazy_static! {
+                static ref RE: Regex = Regex::new(r"^!archive <#(\d+)> ?([\w,]+)?$").unwrap();
+            }
+
+            let capts = RE.captures(&msg.content);
+            if capts.as_ref().map(|x| x.get(0)).is_none() {
+                msg.reply(&ctx, "Invalid syntax.\nCorrect usage is `!archive <channel> [mode(s)]`\nValid modes are `all,json`.").await.expect("Failed to reply to message.");
+                info!("Invalid archive command supplied: '{}'", &msg.content);
+                return;
+            }
+            let capts = capts.unwrap();
+            let channel_id_str = capts.get(1).unwrap().as_str();
+            let modes = match capts
+                .get(2)
+                .map(|x| x.as_str().split(',').collect::<Vec<_>>())
+            {
+                Some(x) => x,
+                None => vec!["all"],
+            };
+
+            let channel = match ChannelId::from_str(channel_id_str) {
+                Ok(x) => x,
+                Err(_) => {
+                    msg.reply(&ctx, format!("Invalid channel id {}.", channel_id_str))
+                        .await
+                        .expect("Failed to reply to message");
+                    return;
+                }
+            }
+            .to_channel(&ctx)
+            .await
+            .expect("Channel not found")
+            .guild()
+            .expect("Invalid channel type");
             let channel_name = channel.name;
             let guild_name = {
                 let guild = channel.guild_id.to_partial_guild(&ctx).await.unwrap();
                 guild.name
             };
             info!(
-                "Archive started by user {} in channel {}",
+                "Archive started by user {} in channel {}, with modes {:?}",
                 msg.author,
                 channel.id.to_string(),
+                modes
             );
             let mut messages: Vec<Message> = Vec::new();
             let mut x = 100;
@@ -86,14 +119,8 @@ impl EventHandler for Handler {
             }
             messages.reverse();
             let messages = messages; // messages is a Vec<Message>, in order from oldest to newest
-
-            match json::write_json(
-                &messages,
-                format!("/dev/shm/{}-{}.json", guild_name, channel_name),
-                &ctx,
-            )
-            .await
-            {
+            let output_filename = format!("{}/{}-{}", PATH, guild_name, channel_name);
+            match json::write_json(&messages, format!("{}.json", output_filename), &ctx).await {
                 Ok(_) => {}
                 Err(x) => error!("Error writing json: {}", x),
             }
